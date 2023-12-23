263deee41a9150856d172bb8b866928024bd4673
Airme1 <emmanuelochoja@yahoo.com>
2023-12-23 02:24:48 +0100

    another linter

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 #Manifest to create a file named school at /tmp
 file { '/tmp/school':
-        owner => 'www-data',
-        group => 'www-data',
-        content => 'I love puppet',
-        mode => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
 }
